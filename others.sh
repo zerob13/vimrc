97633bda2fb5a463bf624e939d567bb82b821314
Lingfeng Yang <i@zerob13.in>
2015-07-11 00:09:26 +0800

    Update others.sh

--- others.sh
+++ others.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 npm install ~/.vim/bundle/tern_for_vim
-npm install -g git://github.com/ramitos/jsctags.git
+npm install -g http://github.com/ramitos/jsctags.git
 npm -g install js-beautify
 npm -g install coffee-formatter-cli
