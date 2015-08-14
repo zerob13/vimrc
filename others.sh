ada30ac9a302fbde758e545cb62f81955ba1ab68
Lingfeng Yang <hulk@juhuiwan.cn>
2015-08-14 15:49:39 +0800

    add typescript support

--- others.sh
+++ others.sh
@@ -4,3 +4,4 @@ npm install ~/.vim/bundle/tern_for_vim
 npm install -g http://github.com/ramitos/jsctags.git
 npm -g install js-beautify
 npm -g install coffee-formatter-cli
+npm install -g typescript-formatter
